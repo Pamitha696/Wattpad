10e78467de4d7ea0cce091c42d1f5bd890ae9f36
Pamitha696 <pamithabootcamp@gmail.com>
2022-11-13 18:10:22 +0530

    3

--- src/com/seekerscloud/pos/controller/SignUpFormController.java
+++ src/com/seekerscloud/pos/controller/SignUpFormController.java
@@ -5,7 +5,13 @@ import com.jfoenix.controls.JFXTextField;
 import com.seekerscloud.pos.db.Database;
 import com.seekerscloud.pos.model.User;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignUpFormController {
     public JFXTextField txtEmail;
@@ -13,31 +19,31 @@ public class SignUpFormController {
     public JFXTextField txtFullName;
     public JFXPasswordField txtContact;
     public JFXPasswordField txtRePassword;
+    public AnchorPane signUpFormContext;
 
-    public void signUpOnAction(ActionEvent actionEvent) {
+    public void signUpOnAction(ActionEvent actionEvent) throws InterruptedException, IOException {
         //check password match
 
-        String realPwd=txtPassword.getText().trim();
-        String matchPwd=txtRePassword.getText().trim();
-        if(!realPwd.equals(matchPwd))
-        {
-            new Alert(Alert.AlertType.WARNING,"Both passwords should match").show();
+        String realPwd = txtPassword.getText().trim();
+        String matchPwd = txtRePassword.getText().trim();
+        if (!realPwd.equals(matchPwd)) {
+            new Alert(Alert.AlertType.WARNING, "Both passwords should match").show();
             return;//we will stop the JVM
         }
-        User u = new User(txtEmail.getText().trim(),txtFullName.getText(),txtContact.getText(),realPwd);
+        User u = new User(txtEmail.getText().trim(), txtFullName.getText(), txtContact.getText(), realPwd);
 
-        if(saveUser(u)){
-            new Alert(Alert.AlertType.CONFIRMATION,"User Registered!").show();
+        if (saveUser(u)) {
+            new Alert(Alert.AlertType.CONFIRMATION, "User Registered!").show();
             clearFields();
-        }
-
-        else{
-            new Alert(Alert.AlertType.WARNING,"Try Again!").show();
+            Thread.sleep(2000);//wait 2 seconds
+            setUi("DashBoardForm",u.getEmail());
+        } else {
+            new Alert(Alert.AlertType.WARNING, "Already Exists!,Try Again!").show();
         }
 
     }
-    private void clearFields()
-    {
+
+    private void clearFields() {
         txtEmail.clear();
         txtFullName.clear();
         txtContact.clear();
@@ -45,8 +51,25 @@ public class SignUpFormController {
         txtRePassword.clear();
     }
 
-    private boolean saveUser(User u){
+    private boolean saveUser(User u) {
+        for (User tempUser: Database.userTable){
+            if (tempUser.getEmail().equals(u.getEmail())){
+                return false;
+            }
+        }
         return Database.userTable.add(u);//inbuilt method ==> java.util
 
+    }
+
+    private void setUi(String location,String title) throws IOException {
+        Stage window = (Stage) signUpFormContext.getScene().getWindow();
+        window.setTitle(title);
+        window.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml")))
+        );
+    }
+
+    public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm","Login Form");
     }
 }
